515a2cedc04d4386c38b6848a7fd7142d904446d
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2022-08-08 20:12:29 +0000

    Add sections to the index file. Add style to the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,58 +5,59 @@ let longitude = 0;
 // let button = document.getElementById("btn")
 // button.addEventListener("click", getWeather)
 
-document.addEventListener("DOMContentLoaded", ()=>{
+// document.addEventListener("DOMContentLoaded", ()=>{
 
-    getLocation();
+//     getLocation();
 
-})
-
-
-
-function getLocation(){
-    if(navigator.geolocation){
-        let x = navigator.geolocation.getCurrentPosition(currentLocation)
-    }
-    else{
-        document.getElementById("test").innerHTML = "Not Supported"
-    }
-}
+// })
 
 
-function currentLocation(location){
-    lat = location.coords.latitude;
-    lon = location.coords.longitude;
-    document.getElementById("test").innerHTML = `Latitude: ${lat}   Longitude: ${lon}`
-    getWeather(lat, lon);
-}
+
+// function getLocation(){
+//     if(navigator.geolocation){
+//         let x = navigator.geolocation.getCurrentPosition(currentLocation)
+//     }
+//     else{
+//         document.getElementById("test").innerHTML = "Not Supported"
+//     }
+// }
 
 
-async function getWeather(lat, lon){
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${lat},${lon}&aqi=no`);
-    const data = await response.json();
-
-    if(response.ok){
-        console.log(data)
-        displayData(data)
-    }
-    else{console.log("Error")}
-}
+// function currentLocation(location){
+//     lat = location.coords.latitude;
+//     lon = location.coords.longitude;
+//     document.getElementById("test").innerHTML = `Latitude: ${lat}   Longitude: ${lon}`
+//     getWeather(lat, lon);
+// }
 
 
-function displayData(data){
+// async function getWeather(lat, lon){
+//     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${lat},${lon}&aqi=no`);
+//     const data = await response.json();
 
-    document.getElementById("name").innerHTML = `Name: ${data.location.name}`;
-    document.getElementById("region").innerHTML = `Region: ${data.location.region}`;
-    document.getElementById("country").innerHTML = `Country: ${data.location.country}`;
-    let loTime = new Date(data.location.localtime_epoch * 1000).toLocaleTimeString();
-    document.getElementById("time").innerHTML = `Time: ${loTime}`;
-    document.getElementById("temp").innerHTML = `Current-Temp: ${data.current.temp_c}C`;
-    document.getElementById("feels-like").innerHTML = `Feels-Like: ${data.current.feelslike_c}C`;
-    document.getElementById("humidity").innerHTML = `Humidity: ${data.current.humidity}`;
-
-}
+//     if(response.ok){
+//         console.log(data)
+//         displayData(data)
+//     }
+//     else{console.log("Error")}
+// }
 
 
+// function displayData(data){
+
+//     document.getElementById("name").innerHTML = `Name: ${data.location.name}`;
+//     document.getElementById("region").innerHTML = `Region: ${data.location.region}`;
+//     document.getElementById("country").innerHTML = `Country: ${data.location.country}`;
+//     let loTime = new Date(data.location.localtime_epoch * 1000).toLocaleTimeString();
+//     document.getElementById("time").innerHTML = `Time: ${loTime}`;
+//     document.getElementById("temp").innerHTML = `Current-Temp: ${data.current.temp_c}C`;
+//     document.getElementById("feels-like").innerHTML = `Feels-Like: ${data.current.feelslike_c}C`;
+//     document.getElementById("humidity").innerHTML = `Humidity: ${data.current.humidity}`;
+
+// }
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
 // unix to loca timestamp
 // dt=new Date(1234567890 * 1000).toLocaleString(); gives date+time — or use .toLocaleDateString() or .toLocaleTimeString()
