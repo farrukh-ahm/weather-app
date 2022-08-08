ef2d3b6af67068c8b11080c666cbdd512bb9e4a5
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2022-08-08 21:04:36 +0000

    Add code to display current weather on the webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,70 @@
 const KEY = "3322850824c94ab79c4164448220508";
 let latitude = 0;
 let longitude = 0;
+let insert;
 
 // let button = document.getElementById("btn")
 // button.addEventListener("click", getWeather)
 
-// document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
 
-//     getLocation();
+    getLocation();
 
-// })
-
-
-
-// function getLocation(){
-//     if(navigator.geolocation){
-//         let x = navigator.geolocation.getCurrentPosition(currentLocation)
-//     }
-//     else{
-//         document.getElementById("test").innerHTML = "Not Supported"
-//     }
-// }
+})
 
 
-// function currentLocation(location){
-//     lat = location.coords.latitude;
-//     lon = location.coords.longitude;
-//     document.getElementById("test").innerHTML = `Latitude: ${lat}   Longitude: ${lon}`
-//     getWeather(lat, lon);
-// }
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(currentLocation)
+    }
+    else{
+        document.getElementById("test").innerHTML = "Not Supported"
+    }
+}
 
 
-// async function getWeather(lat, lon){
-//     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${lat},${lon}&aqi=no`);
-//     const data = await response.json();
-
-//     if(response.ok){
-//         console.log(data)
-//         displayData(data)
-//     }
-//     else{console.log("Error")}
-// }
+function currentLocation(location){
+    lat = location.coords.latitude;
+    lon = location.coords.longitude;
+    getWeather(lat, lon);
+}
 
 
-// function displayData(data){
+async function getWeather(lat, lon){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${lat},${lon}&aqi=no`);
+    const data = await response.json();
 
-//     document.getElementById("name").innerHTML = `Name: ${data.location.name}`;
-//     document.getElementById("region").innerHTML = `Region: ${data.location.region}`;
-//     document.getElementById("country").innerHTML = `Country: ${data.location.country}`;
-//     let loTime = new Date(data.location.localtime_epoch * 1000).toLocaleTimeString();
-//     document.getElementById("time").innerHTML = `Time: ${loTime}`;
-//     document.getElementById("temp").innerHTML = `Current-Temp: ${data.current.temp_c}C`;
-//     document.getElementById("feels-like").innerHTML = `Feels-Like: ${data.current.feelslike_c}C`;
-//     document.getElementById("humidity").innerHTML = `Humidity: ${data.current.humidity}`;
+    if(response.ok){
+        console.log(data)
+        displayData(data)
+    }
+    else{console.log("Error")}
+}
 
-// }
+
+function displayData(data){
+    insert = "";
+    insert = `<div id="temp"><h2>${data.current.temp_c} C</h2></div>`
+    insert += `<p id="feels">Feels Like: ${data.current.feelslike_c} C</p>`
+    insert += `<p id="humidity">Humidity: ${data.current.humidity}</p>`
+    insert += `<div id="condition"><img src="${data.current.condition.icon}"><p>${data.current.condition.text}</p></div>`
+    insert += `<p id="wind">Wind: ${data.current.wind_kph}kph</p>`
+    insert += `<p id="location">${data.location.name}, ${data.location.region}, ${data.location.country}</p>`
+    let loTime = new Date(data.location.localtime_epoch * 1000).toLocaleTimeString();
+    insert += `<p id="time">${loTime}</p>`
+    document.getElementById("container").innerHTML = insert;
+
+    // document.getElementById("name").innerHTML = `Name: ${data.location.name}`;
+    // document.getElementById("region").innerHTML = `Region: ${data.location.region}`;
+    // document.getElementById("country").innerHTML = `Country: ${data.location.country}`;
+    
+    // document.getElementById("time").innerHTML = `Time: ${loTime}`;
+    // document.getElementById("temp").innerHTML = `Current-Temp: ${data.current.temp_c}C`;
+    // document.getElementById("feels-like").innerHTML = `Feels-Like: ${data.current.feelslike_c}C`;
+    // document.getElementById("humidity").innerHTML = `Humidity: ${data.current.humidity}`;
+
+}
 
 
 // --------------------------------------------------------------------------------------------------------------------------------------------
