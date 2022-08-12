2097585c3d9ab64cf86e762759d61cfaf2a39b85
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2022-08-12 08:07:13 +0000

    Add error handling function for geolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 function getLocation(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(currentLocation)
+        navigator.geolocation.getCurrentPosition(currentLocation, errorCode)
     }
     else{
         document.getElementById("test").innerHTML = "Not Supported"
@@ -65,6 +65,23 @@ function displayTime(){
     document.getElementById("time").innerText = currentTime.toLocaleTimeString();
 }
 
+
+function errorCode(error){
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for GeoLocation");
+            break
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable");
+            break
+        case error.TIMEOUT:
+            alert("The request timed out");
+            break
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occured");
+            break
+    }
+}
 
 // --------------------------------------------------------------------------------------------------------------------------------------------
 
